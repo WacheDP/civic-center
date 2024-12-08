192779eda5f476e3a2e485922ab18130f597bf29
Daniel Parra <wacheparra21@gmail.com>
2024-12-08 18:32:27 -0400

    Departments

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -25,3 +25,28 @@ export const getEmployee = async (req, res) => {
     if (rows.length == 0) { return res.status(404).json({ message: "User not found" }) }
     res.json(rows[0])
 }
+
+export const deleteEmployee = async (req, res) => {
+    const { id } = req.params
+    const { rowCount } = await pool.query(`delete from employees where id=${id}`)
+
+    if (rowCount == 0) { return res.status(404).json({ message: "User not found" }) }
+
+    res.sendStatus(204)
+}
+
+export const updateEmployee = async (req, res) => {
+    const { id } = req.params
+    const { ci, rif, first_name, middle_name, last_name, surname, address, phone } = req.body
+    const { birth_date, start_date, nationality, blood_type, allergies, department, photo } = req.body
+
+    const cadena = `update employees set ci=${ci}, rif=${rif}, first_name=${first_name}, middle_name=${middle_name}`
+    cadena += `, last_name=${last_name}, surname=${surname}, address=${address}, phone=${phone}, `
+    cadena += `birth_date=${birth_date}, start_date=${start_date}, nationality=${nationality}, `
+    cadena += `blood_type=${blood_type}, allergies=${allergies}, department=${department}, photo=${photo} `
+    cadena += `where id=${id}`
+
+    const { rows } = await pool.query(cadena)
+
+    res.json(rows[0])
+}
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import { getEmployee, getEmployees, postEmployee } from '../controllers/employees'
+import { deleteEmployee, getEmployee, getEmployees, postEmployee, updateEmployee } from '../controllers/employees'
 
 const router = Router()
-
 router.post('/employees', postEmployee)
-
 router.get('/employees', getEmployees)
-
 router.get('/employees/:id', getEmployee)
+router.delete('/users/:id', deleteEmployee)
+router.put('/users/:id', updateEmployee)
 
 export default router
